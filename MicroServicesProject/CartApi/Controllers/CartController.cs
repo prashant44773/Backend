a9352d286d10b7bd1f0b56b31e7e1675ad0a332c
Prashant Naiker <prashant.naiker@ispl.com>
2023-06-02 18:31:49 +0530

    .NET Backend Of E-Commerce Api

---
+++ MicroServicesProject/CartApi/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using CartApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CartApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        // GET: api/<CartController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST api/<CartController>
+        [HttpPost("Post")]
+        public void Post([FromBody] Cart add)
+        {
+        }
+
+        // PUT api/<CartController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CartController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MicroServicesProject/CartApi/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    public class Cart
+    {
+        public int UserID { get; set; } = 1;
+        public int ProductID { get; set; }
+        public int Price { get; set; }
+        public int RequestID { get; set; }
+    }
+}
